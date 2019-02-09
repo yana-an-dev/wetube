a32f98db5c31c29eb024c58bd87da8d520ea42c2
yaninbkk <37002930+yaninbkk@users.noreply.github.com>
2019-02-09 19:39:32 +0900

    nodemon

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-const express = require("express");
+require ("@babel/register");
+import express from "express";
 const app = express();
 
 const PORT = 4000;
 
-function handleListening(){
-    console.log('Listening on: http://localhost:${PORT}');
-    }
-
-function handleHome(req, res){
-    res.send("hello from home");
-}
-
-function handleProfile(req, res){
-    res.send("You are my profile");
-}^
+const handleListening = ()=>
+console.log('Listening on: http://localhost:${PORT}');
+const handleHome = (req, res) => res.send("hello from home 2");
+const handleProfile = (req, res) => res.send("You are my love");
 
 app.get("/", handleHome);
 
