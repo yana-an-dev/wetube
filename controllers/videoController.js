3351ae4016ae6016a50aa22f17e5991377541fa1
yaninbkk <37002930+yaninbkk@users.noreply.github.com>
2019-03-05 09:40:46 +0900

    #2.22 Home Controller part Two
    error fixed, mixins

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -7,9 +7,6 @@ export const search = (req, res) => {
     const {query: { term: searchingBy }} = req;
     res.render("search", { pageTitle:"Search", searchingBy});
 };
-export const videos =(req, res) => 
-    res.render("videos", { pageTitle:"Videos"});
-
 export const upload =(req, res) => 
     res.render("upload", { pageTitle:"Upload"});
 
