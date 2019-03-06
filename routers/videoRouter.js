3818301bfc88d3a5038fde9dda32d09aa49ecb9f
yaninbkk <37002930+yaninbkk@users.noreply.github.com>
2019-03-06 10:48:10 +0900

    #2.25 More Controlers
    Cannot GET /video/151515

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,7 +1,6 @@
 import express from "express";
 import routes from "../routes";
 import { 
-    //videos, 
     upload, 
     videoDetail, 
     editVideo, 
@@ -10,9 +9,8 @@ import {
 
 const videoRouter = express.Router();
 
-//videoRouter.get(routes.videos, videos);
 videoRouter.get(routes.upload, upload);
-videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.videoDetail(), videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);
 
