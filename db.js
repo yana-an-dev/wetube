22ddf8771ba33e3c02f604ba42cf643905599ec6
yaninbkk <37002930+yaninbkk@users.noreply.github.com>
2019-03-11 00:30:51 +0900

    #3.1 Connecting to MongoDB

--- db.js
+++ db.js
@@ -1,50 +1,17 @@
-export const videos = [
-    {
-      id: 151515,
-      title: "Video awesome",
-      description: "This is something I love",
-      views: 24,
-      videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-      creator: {
-        id: 121212,
-        name: "Yana",
-        email: "Yana@g.mail"
-      }
-    },
-    {
-      id: 454545,
-      title: "Video super",
-      description: "This is something I love",
-      views: 24,
-      videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-      creator: {
-        id: 121212,
-        name: "Yana",
-        email: "Yana@g.mail"
-      }
-    },
-    {
-      id: 969696,
-      title: "Video nice",
-      description: "This is something I love",
-      views: 24,
-      videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-      creator: {
-        id: 121212,
-        name: "Yana",
-        email: "Yana@g.mail"
-      }
-    },
-    {
-      id: 363636,
-      title: "Video perfect",
-      description: "This is something I love",
-      views: 24,
-      videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-      creator: {
-        id: 121212,
-        name: "Yana",
-        email: "Yana@g.mail"
-      }
-    }
-  ];
+import mongoose from "mongoose";
+
+mongoose.connect(
+  "mongodb://localhost:27017/we-tube",
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false
+  }
+);
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅  Connected to DB");
+const handleError = error => console.log(`❌ Error on DB Connection:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
