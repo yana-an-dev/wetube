427d8c6cfbdf990e2f4120381a2c8e8160d46b2d
yaninbkk <37002930+yaninbkk@users.noreply.github.com>
2019-03-11 23:21:24 +0900

    #3.4 Comment Model
    fixed. no problem of code, but I need to turn on mongod through:
    
    cd mongod/bin/
    mongod.exe --config mongod.cfg

--- init.js
+++ init.js
@@ -3,10 +3,11 @@ import app from "./app";
 import dotenv from "dotenv";
 dotenv.config();
 import "./models/Video";
+import "./models/Comment";
 
 const PORT = process.env.PORT || 4000;
 
 const handleListening = () => 
-    console.log("😆 Listening on: http://localhost:$(PORT)");
+    console.log("😆 Listening on: http://localhost:${PORT}");
 
 app.listen(PORT, handleListening);
