343e1661ee67667e7ff814a516ca58249429fd78
yaninbkk <37002930+yaninbkk@users.noreply.github.com>
2019-03-11 23:52:46 +0900

    #3.5 Home Controller Finished

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,16 @@
 import routes from "../routes";
-export const home = (req, res) => {
-    res.render("home", { pageTitle:"Home", videos});
-}
+import Video from "../models/Video";
+
+export const home = async(req, res) => {
+    try {
+        const videos = await Video.find({});
+        res.render("home", { pageTitle:"Home", videos});
+    } catch (error) {
+        console.log(error);
+        res.render("home", { pageTitle:"Home", videos:[] });
+    }
+};
+
 export const search = (req, res) => {
    // const searchingby =req.query.term;과 같다. 
     const {query: { term: searchingBy }} = req;
