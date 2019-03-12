fa2dd7c35b782f6c0585a448d67716bfabbfb12b
yaninbkk <37002930+yaninbkk@users.noreply.github.com>
2019-03-12 10:15:08 +0900

    #3.6 Uploading and Creating a Video

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -19,17 +19,24 @@ export const search = (req, res) => {
 export const getUpload =(req, res) => 
     res.render("upload", { pageTitle:"Upload"});
 
-export const postUpload =(req, res) => {
-    const{
-        body:{ file, title, description }
-    } = req;
-    //Todo:Upload and save video
-    res.redirect(routes.videoDetail(151515))
+export const postUpload = async (req, res) => {
+    const{ 
+        body: {title, description},
+        file: {path}
+     } = req;
+    const newVideo = await Video.create({
+        fileUrl: path,
+        title,
+        description
+    })
+    
+    res.render("upload", { pageTitle:"Upload"});
+    res.redirect(routes.videoDetail(newVideo.id));
 };
 
 
-    export const videoDetail= (req, res) => 
-    res.render("videoDetail", { pageTitle:"Video Detail"});
+export const videoDetail= (req, res) => 
+res.render("videoDetail", { pageTitle:"Video Detail"});
 
 export const editVideo=(req, res) => 
     res.render("editVideo", { pageTitle:"Edit Video"});
