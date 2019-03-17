e8d1bbe03a071b9f4eb94513b72f965ec21ed809
yaninbkk <37002930+yaninbkk@users.noreply.github.com>
2019-03-17 13:44:00 +0900

    #4.0 Introduction to Webpack

--- middlewares.js
+++ middlewares.js
@@ -1,16 +1,16 @@
 import multer from "multer";
 import routes from "./routes";
 
-const multerVideo = multer({dest: "uploads/videos/"});
+const multerVideo = multer({ dest: "uploads/videos/" });
 
 export const localsMiddelware = (req, res, next) => {
-    res.locals.siteName = "YanaTube";
-    res.locals.routes = routes;
-    res.locals.user ={
-        isAuthenticated: true,
-        id:1
-    };
-    next();
+  res.locals.siteName = "YanaTube";
+  res.locals.routes = routes;
+  res.locals.user = {
+    isAuthenticated: true,
+    id: 1
+  };
+  next();
 };
 
 export const uploadVideo = multerVideo.single("videoFile");
