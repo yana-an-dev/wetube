d066462ee6374d0f310104ae564bca007ef628fe
yaninbkk <37002930+yaninbkk@users.noreply.github.com>
2019-03-18 23:10:48 +0900

    #4.2 Styles with Webpack part Two

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,38 @@
 const path = require("path");
-const ExtractCSS = require("extract-text-webpack-plugin";)
+const autoprefixer = require("autoprefixer");
+const ExtractCSS = require("extract-text-webpack-plugin");
 
 const MODE = process.env.WEBPACK_ENV;
 const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
 
 const config = {
-  entry: ENTRY_FILE,
+  entry: ["@babel/polyfill", ENTRY_FILE],
   mode: MODE,
   module: {
     rules: [
+      // {
+      //   test: /\.(js)$/,
+      //   use: [
+      //     {
+      //       loader: "babel-loader"
+      //     }
+      //   ]
+      // },
       {
-        test: /\(.scss)$/,
+        test: /\.(scss)$/,
         use: ExtractCSS.extract([
           {
             loader: "css-loader"
-        },
-        {
-            loader: "postcss-loader"
-        },
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins() {
+                return [autoprefixer({ browsers: "cover 99.5%" })];
+              }
+            }
+          },
           {
             loader: "sass-loader"
           }
@@ -28,8 +42,9 @@ const config = {
   },
   output: {
     path: OUTPUT_DIR,
-    filename: "[name].[format]"
-  }
+    filename: "[name].js"
+  },
+  plugins: [new ExtractCSS("styles.css")]
 };
 
 module.exports = config;
