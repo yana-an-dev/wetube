91ea5226dcdeedae6bb9d2a6f0d4386afb20e2d2
yaninbkk <37002930+yaninbkk@users.noreply.github.com>
2019-03-26 09:33:01 +0900

    #6.7 Github Log In part Two
    errors
    - log out
    - github login

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,14 +1,17 @@
 import express from "express";
 import routes from "../routes";
+import passport from "passport";
 import { home, search } from "../controllers/videoController";
 import {
   getJoin,
   postJoin,
   logout,
   getLogin,
-  postLogin
+  postLogin,
+  githubLogin,
+  postGithubLogIn
 } from "../controllers/userController";
-import { onlyPublic } from "../middlewares";
+import { onlyPublic, onlyPrivate } from "../middlewares";
 
 const globalRouter = express.Router();
 
@@ -20,6 +23,14 @@ globalRouter.post(routes.login, onlyPublic, postLogin);
 
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
-globalRouter.get(routes.logout, onlyPublic, logout);
+globalRouter.get(routes.logout, onlyPrivate, logout);
+
+globalRouter.get(routes.github, githubLogin);
+
+globalRouter.get(
+  routes.githubCallback,
+  passport.authenticate("github", { failureRedirect: "/login" }),
+  postGithubLogIn
+);
 
 export default globalRouter;
--- routes.js
+++ routes.js
@@ -8,7 +8,7 @@ const SEARCH = "/search";
 // Users
 
 const USERS = "/users";
-const EDIT_PROFILE ="/edit-profile";
+const EDIT_PROFILE = "/edit-profile";
 const USER_DETAIL = "/:id";
 const CHANGE_PASSWORD = "/change-password";
 
@@ -16,49 +16,56 @@ const CHANGE_PASSWORD = "/change-password";
 
 const VIDEOS = "/videos";
 const UPLOAD = "/upload";
-const VIDEO_DETAIL ="/:id";
+const VIDEO_DETAIL = "/:id";
 const EDIT_VIDEO = "/:id/edit";
 const DELETE_VIDEO = "/:id/delete";
 
+// Github
+
+const GITHUB = "/auth/github";
+const GITHUB_CALLBACK = "/auth/github/callback";
+
 const routes = {
- home: HOME,
- join: JOIN,
- login: LOGIN,
- logout: LOGOUT,
- search: SEARCH,
- users: USERS,
- userDetail: id => {
-     if (id) {
-         return `/users/${id}`;
-     } else {
-         return USER_DETAIL; 
-     }
-    },
- editProfile: EDIT_PROFILE,
- changePassword: CHANGE_PASSWORD,
- videos: VIDEOS,
- upload: UPLOAD,
- videoDetail: id => {
-     if(id) {
-         return `/videos/${id}`;
-     } else {
-         return VIDEO_DETAIL;
-     }
-    },
- editVideo: id=>{
-     if (id){
-         return `/videos/${id}/edit`
-     } else {
-         return EDIT_VIDEO;
-     }
- },
- deleteVideo: id => {
-     if (id) {
-         return `/videos/${id}/delete`;
-     } else {
-         return DELETE_VIDEO;
-     }
+  home: HOME,
+  join: JOIN,
+  login: LOGIN,
+  logout: LOGOUT,
+  search: SEARCH,
+  users: USERS,
+  userDetail: id => {
+    if (id) {
+      return `/users/${id}`;
+    } else {
+      return USER_DETAIL;
     }
+  },
+  editProfile: EDIT_PROFILE,
+  changePassword: CHANGE_PASSWORD,
+  videos: VIDEOS,
+  upload: UPLOAD,
+  videoDetail: id => {
+    if (id) {
+      return `/videos/${id}`;
+    } else {
+      return VIDEO_DETAIL;
+    }
+  },
+  editVideo: id => {
+    if (id) {
+      return `/videos/${id}/edit`;
+    } else {
+      return EDIT_VIDEO;
+    }
+  },
+  deleteVideo: id => {
+    if (id) {
+      return `/videos/${id}/delete`;
+    } else {
+      return DELETE_VIDEO;
+    }
+  },
+  github: GITHUB,
+  githubCallback: GITHUB_CALLBACK
 };
 
 export default routes;
