ceecf618a411c3a7af341069897f8dcb64c197e4
yaninbkk <37002930+yaninbkk@users.noreply.github.com>
2019-04-03 10:18:05 +0900

    #7.1 Edit User Password
    Edit user name and update profile, not changed password

--- controllers/userController.js
+++ controllers/userController.js
@@ -116,7 +116,27 @@ export const userDetail = async (req, res) => {
     res.redirect(routes.home);
   }
 };
-export const editProfile = (req, res) =>
+
+export const getEditProfile = (req, res) =>
   res.render("editProfile", { pageTitle: "Edit Profile" });
+
+export const postEditProfile = async (req, res) => {
+  const {
+    body: { name, email },
+    file
+  } = req;
+  try {
+    console.log([req.user.id, name, email]);
+    await User.findByIdAndUpdate(req.user.id, {
+      name,
+      email,
+      avatarUrl: file ? file.path : req.user.avatarUrl
+    });
+    res.redirect(routes.me);
+  } catch (error) {
+    res.render("editProfile", { pageTitle: "Edit Profile" });
+  }
+};
+
 export const changePassword = (req, res) =>
   res.render("changePassword", { pageTitle: "Change Password" });
